e866c0699074fef75338803d04cbfe4c47287d90
Alexander Taran <taransasha004@gmail.com>
2024-06-17 20:27:07 +0300

    Adding logs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -18,6 +19,7 @@ type RingBuffer struct {
 }
 
 func NewRingBuffer(size int) *RingBuffer {
+	log.Printf("Creating new ring buffer of size %d\n", size)
 	return &RingBuffer{make([]int, size), size, -1, sync.Mutex{}}
 }
 
@@ -34,6 +36,7 @@ func (rb *RingBuffer) Push(el int) {
 		rb.position++
 		rb.buffer[rb.position] = el
 	}
+	log.Printf("Pushed element %d to ring buffer\n", el)
 }
 
 func (rb *RingBuffer) Get() []int {
@@ -46,6 +49,7 @@ func (rb *RingBuffer) Get() []int {
 	var output []int = rb.buffer[:rb.position+1]
 	rb.position = -1
 
+	log.Printf("Got elements from ring buffer: %v\n", output)
 	return output
 }
 
@@ -55,16 +59,19 @@ func read(nextStage chan<- int, done chan<- bool) {
 	for scanner.Scan() {
 		data = scanner.Text()
 		if strings.EqualFold(data, "exit") {
+			log.Println("Program is exiting")
 			fmt.Println("Программа завершила работу!")
 			close(done)
 			return
 		}
 		i, err := strconv.Atoi(data)
 		if err != nil {
+			log.Printf("Invalid number: %s\n", data)
 			fmt.Println("Некорректное число")
 			continue
 		}
 		nextStage <- i
+		log.Printf("Read number %d from input\n", i)
 	}
 }
 
@@ -74,8 +81,12 @@ func negatives(input <-chan int, output chan<- int, done <-chan bool) {
 		case data := <-input:
 			if data > 0 {
 				output <- data
+				log.Printf("Sent positive number %d to output\n", data)
+			} else {
+				log.Printf("Ignored non-positive number %d\n", data)
 			}
 		case <-done:
+			log.Println("Negatives function is exiting")
 			return
 		}
 	}
@@ -87,8 +98,12 @@ func notDividedThreeFunc(input <-chan int, output chan<- int, done <-chan bool) 
 		case data := <-input:
 			if data%3 == 0 {
 				output <- data
+				log.Printf("Sent number %d divisible by 3 to output\n", data)
+			} else {
+				log.Printf("Ignored number %d not divisible by 3\n", data)
 			}
 		case <-done:
+			log.Println("NotDividedThree function is exiting")
 			return
 		}
 	}
@@ -100,12 +115,15 @@ func bufferStageFunc(input <-chan int, output chan<- int, done <-chan bool, size
 		select {
 		case data := <-input:
 			buffer.Push(data)
+			log.Printf("Buffered number %d\n", data)
 		case <-time.After(interval):
 			bufferData := buffer.Get()
 			for _, data := range bufferData {
 				output <- data
+				log.Printf("Sent buffered number %d to output\n", data)
 			}
 		case <-done:
+			log.Println("BufferStage function is exiting")
 			return
 		}
 	}
@@ -130,8 +148,10 @@ func main() {
 	for {
 		select {
 		case data := <-bufferedIntChannel:
+			log.Printf("Processed data: %d\n", data)
 			fmt.Println("Обработанные данные: ", data)
 		case <-done:
+			log.Println("Main function is exiting")
 			return
 		}
 	}
